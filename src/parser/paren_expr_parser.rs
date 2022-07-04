3988f0cf434222dd8b331a716a973c9b341dcd6d
JichouP <akkunn100@gmail.com>
2022-07-04 11:28:07 +0900

    :white_check_mark: add test

--- src/parser/paren_expr_parser.rs
+++ src/parser/paren_expr_parser.rs
@@ -5,7 +5,15 @@ use super::expr_parser::expr_parser;
 
 pub fn paren_expr_parser(input: &str) -> IResult<&str, Expr> {
     let (unused, _) = char('(')(input)?;
-    let (unused, expr) = expr_parser(input)?;
-    let (unused, _) = char(')')(input)?;
+    let (unused, expr) = expr_parser(unused)?;
+    let (unused, _) = char(')')(unused)?;
     Ok((unused, expr))
+}
+
+#[test]
+fn paren_expr_parser_test() {
+    use crate::domain::expr::{constant_val::ConstantVal, Expr};
+    let (_, actual) = paren_expr_parser("(14)").unwrap();
+    let expect = Expr::ConstantVal(ConstantVal::new(14));
+    assert_eq!(actual, expect);
 }
