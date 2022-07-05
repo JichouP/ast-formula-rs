fb821ef9c722c028cca6d44cfb102d58afa1893c
JichouP <akkunn100@gmail.com>
2022-07-05 18:23:56 +0900

    :sparkles: add space0 to paren_expr_parser

--- src/parser/paren_expr_parser.rs
+++ src/parser/paren_expr_parser.rs
@@ -1,19 +1,21 @@
-use crate::domain::expr::Expr;
-use nom::{character::complete::char, IResult};
-
 use super::expr_parser::expr_parser;
+use crate::domain::expr::Expr;
+use nom::{
+    character::complete::{char, space0},
+    sequence::tuple,
+    IResult,
+};
 
 pub fn paren_expr_parser(input: &str) -> IResult<&str, Expr> {
-    let (unused, _) = char('(')(input)?;
-    let (unused, expr) = expr_parser(unused)?;
-    let (unused, _) = char(')')(unused)?;
+    let (unused, (_, _, expr, _, _)) =
+        tuple((char('('), space0, expr_parser, space0, char(')')))(input).unwrap();
     Ok((unused, expr))
 }
 
 #[test]
 fn paren_expr_parser_test() {
     use crate::domain::expr::{constant_val::ConstantVal, Expr};
-    let (_, actual) = paren_expr_parser("(14)").unwrap();
+    let (_, actual) = paren_expr_parser("( 14 )").unwrap();
     let expect = Expr::ConstantVal(ConstantVal::new(14));
     assert_eq!(actual, expect);
 }
