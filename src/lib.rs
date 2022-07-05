2bb8dece7b8f1bdeca5a5d9e746f6f0a49cea58e
JichouP <akkunn100@gmail.com>
2022-07-05 19:24:07 +0900

    :sparkles: add calc function

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
+use parser::expr_parser::expr_parser;
+
 pub mod domain;
 pub mod parser;
 
-pub fn add(left: usize, right: usize) -> usize {
-    let a = domain::expr::constant_val::ConstantVal::new(1);
-    println!("{}", a.eval());
-    left + right
+pub fn calc(input: &str) -> i32 {
+    let (_, expr) = expr_parser(input).expect("syntax error");
+    expr.eval()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::calc;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = calc("34 + 5 * 6 / 2 - 2 - 1");
+        assert_eq!(result, 34 + 5 * 6 / 2 - 2 - 1);
+    }
+
+    #[test]
+    #[should_panic = "syntax error"]
+    fn should_panic() {
+        calc("a1+2");
     }
 }
